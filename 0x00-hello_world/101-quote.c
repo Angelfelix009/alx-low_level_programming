b52724676a0a695ec8d87d902f708ba4e8f4974a
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-13 12:44:22 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
 /**
+ * main -Program  entry point
  *
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * Return:1 on success.
  */
 int main(void)
 {
-		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-			write(2, str1, 59);
-				return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
