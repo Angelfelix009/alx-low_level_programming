c0169c776cf163fc2d3cdc9cd1399d6b875041c2
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-13 11:54:28 -0700

    put betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * Main Function of the program
+ * It Serves as aentry point
+ * */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
