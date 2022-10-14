494fb4ab6eb6b4c6cb34c926175de9a497561290
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-14 14:40:08 -0700

    If Generate

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	i = n % 10;
 	if (i > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n,i);
+		printf("Last digit of %d is %d and is greater than 5\n", n, i);
 	}
 	else if (i == 0)
 	{
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else if (i < 6 && i != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,i);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
 	}
 	return (0);
 }
