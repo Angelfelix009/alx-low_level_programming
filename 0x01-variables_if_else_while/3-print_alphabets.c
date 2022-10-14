e8a8b83ef72827f14f801812493ff845409da5bb
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-14 15:01:56 -0700

    this i

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-program entry point
+ * Return:0 no error, non zero if error
+ */
+int main(void)
+{
+	char i, j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
