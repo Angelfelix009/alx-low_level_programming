5505251b790da8d72e8793d589ec3df3a9df2337
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-14 16:15:02 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-Program entry point
+ * Return:0  no error, non zero error
+ */
+int main(void)
+{
+	int i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
