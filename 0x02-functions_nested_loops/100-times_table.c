2758a680c3dd1fe9c902e8c4d4fdfe23900fa4b7
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-18 15:09:52 -0700

    loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,13 +7,12 @@
 void print_times_table(int n)
 {
 	int a, b, op;
-	
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
 		{
 			_putchar(48);
-			for (b =1; b <=n; b++)
+			for (b = 1; b <= n; b++)
 			{
 				op = a * b;
 				_putchar(44);
@@ -35,7 +34,7 @@ void print_times_table(int n)
 					_putchar(((op / 100) % 10) + 48);
 					_putchar(((op / 10) % 10) + 48);
 					_putchar((op % 10) + 48);
-				}	
+				}
 			}
 			_putchar('\n');
 		}
