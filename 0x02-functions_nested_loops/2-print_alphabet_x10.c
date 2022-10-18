6143b1507a8ac29b04718bd132ff28799cc92753
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-18 12:58:08 -0700

    nexted

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10-print alphabelt x10
+ * Return:On success 1
+ */
+void print_alphabet_x10(void)
+{
+	char i, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
