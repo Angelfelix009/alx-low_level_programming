158a89262d890132a0d23204f43ae4c4d467312a
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-18 14:11:09 -0700

    loop

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * jack_bauer-prints all minutes in a day from 00:00
+ * Return:0 if successful
+ */
+void jack_bauer(void)
+{
+	int i, j, k, m;
+
+	for (j = 0; j <= 2; j++)
+	{
+		for (i = 0; i < = 9; i++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					if (j ==2 && i > 3)
+					{
+						break;
+					}
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(m + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
