b1ab963a4e7591cdd803fa986a5ed9c0161a598a
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-18 13:08:39 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower-check for lower case
+ * Return:0-if lowercase, 1-if not.
+ */
+int _islower(int c)
+{
+	if(c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
