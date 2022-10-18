dd94e5a5546eb5803b3050865531acd26dbc6a79
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-18 15:05:44 -0700

    loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,7 +34,7 @@ void print_times_table(int n)
 				{
 					_putchar(((op / 100) % 10) + 48);
 					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48)
+					_putchar((op % 10) + 48);
 				}	
 			}
 			_putchar('\n');
