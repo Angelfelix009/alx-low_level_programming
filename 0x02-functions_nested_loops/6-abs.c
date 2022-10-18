03ad59640db0fd29836389cc2f30ae03e36a13c2
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-18 13:37:06 -0700

    absolute valure

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs-computes absolute valu
+ * @a:the integer to compute its absolute value
+ * Return:returns tha abs value
+ */
+int _abs(int a)
+{
+	int k = a, absvalue;
+
+	if (k < 0)
+	{
+		k = k * (-1);
+	}
+	absvalue = k;
+	return (absvalue)
+}
