9d47a9af4ce210f9ff5e72a40578a521019836ec
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-18 12:30:10 -0700

    Alphalbelt print

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet-Print all alphabelt
+ * Return: on success 1
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a < = 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n);
+}
