15f07aac551f54e98561427237868934801666a1
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-19 05:04:49 -0700

    dkdd

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,8 +6,10 @@
 int main(void)
 {
 	int c, boolean, boolean2;
-	long int n2 = 2, n1 = 1, fn2, fn, n11, n22;
+	long int n1, n2, fn, fn2, n11, n22;
 	
+	n1 = 1;
+	n2 = 2;
 	boolean =  boolean2 = 1;
 	printf("%ld, %ld", n1, n2);
 	for (c = 0; c < 96; c++)
