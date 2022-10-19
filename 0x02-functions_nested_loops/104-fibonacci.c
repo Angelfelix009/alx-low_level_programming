c3cf7cf6e1b46f138ccc835991cbb4b7ebe64e56
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-19 04:51:58 -0700

    dkdd

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -31,14 +31,14 @@ int main(void)
 				n2 = n2 / 1000000000;
 				boolean2 = 0;
 			}
-			fn2 = n11 + n22);
-		       fn = n1 + n2 + (fn2 / 1000000000);
-		       printf(", %ld", fn);
-		       printf("%ld", fn2 % 1000000000);
-		       n1 = n2;
-		       n11 = n22;
-		       n2 = fn;
-		       n22 = (fn2 % 1000000000);
+			fn2 = (n11 + n22);
+			fn = n1 + n2 + (fn2 / 1000000000);
+			printf(", %ld", fn);
+			printf("%ld", fn2 % 1000000000);
+			n1 = n2;
+			n11 = n22;
+			n2 = fn;
+			n22 = (fn2 % 1000000000);
 		}
 		if (((n1 + n2) < 0) && boolean == 1)
 		{
