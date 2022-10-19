de6cd1f4fa8f0f233ed477582c0d4ff9e1065a4f
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-19 03:24:35 -0700

    add even

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main-Prints the addition of the even valued fibonacci numbers
+ * Return:ALways 0
+ */
+int main(void)
+{
+	long int n1 = 1, n2 =2, fn, afn;
+
+	fn = afn =0;
+	while (fn <= 4000000)
+	{
+		fn = n1 + n2;
+		n1 = n2;
+		if ((n1 % 2) ==0)
+		{
+			afn += n1;
+		}
+	}
+	printf("%d\n", afn);
+	return (0);
+}
