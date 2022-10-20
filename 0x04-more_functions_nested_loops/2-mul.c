0cf22ef8e8b7b8d98d31d07c069dae5ef4e20f61
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-20 09:11:01 -0700

    multiplication file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul: Multiplies two integers
+ * @a: input to multiply with
+ * @b: input to multiply with
+ * Return: mutiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
