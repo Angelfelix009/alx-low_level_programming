09d1e07c6898e191f346cdce1e3da048185f247a
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-20 21:05:08 -0700

    tria

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,13 +12,11 @@ void print_triangle(int size)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 1; j < (size - i); j++)
-		{
 			_putchar(' ');
-			for (j--; j < size; j++)
-				_putchar(35);
-			if (i < (size - 1))
-				_putchar('\n');
-		}
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
 	}
 	_putchar('\n');
 }
