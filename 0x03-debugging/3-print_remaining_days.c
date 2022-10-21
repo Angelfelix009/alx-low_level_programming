2b02cc5629ecb0be216c674d82608ef61459fce9
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-20 22:03:56 -0700

    d

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -25,7 +25,7 @@ void print_remaining_days(int month, int day, int year)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-		
+
 		else
 		{
 			printf("Day of the year: %d\n", day);
