2196f09ee281681cdcd70cdbf339091d4d9a46c3
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-24 11:43:02 -0700

    Reverse direction

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev- prints a string in reverse
+ * @s: string to be printed 
+ * return: no return
+ */
+void print_rev(char *s);
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
+}
