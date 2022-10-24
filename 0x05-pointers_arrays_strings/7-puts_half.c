bd6c6441080105bcbf3133bc6e9907b037ccce25
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-24 12:13:27 -0700

    r

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half- prints half of a string
+ * @str: string to print
+ * Return: no return
+ */
+void puts_half(char *str)
+{
+	int count = 0, i;
+	whule (count >= 0)
+	{
+		if (str[count] == '\0')
+			break;
+		count++;
+	}
+	if (count % 2 == 1)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+	for (i++; i < count; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
