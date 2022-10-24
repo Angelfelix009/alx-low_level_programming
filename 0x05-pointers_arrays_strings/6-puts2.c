2184d580eba8395d657fdae64d6f3911ae7f67d7
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-24 12:08:09 -0700

    reverse

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2- prints every other character of a string, starting with the first character
+ * puts2- prints every other character out of 2 of a string
  * @str: character to be printer
  * Return: no return
  */
