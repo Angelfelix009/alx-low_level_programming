e0bd42b2197ba698d45faf63d90ba82b0f560981
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-27 06:29:16 -0700

    s

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,7 +10,7 @@ char *leet(char *s)
 	int count = 0, i;
 	int low_letters[] = {97, 101, 111, 116, 108};
 	int upp_letters[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 66, 49};
+	int numbers[] = {52, 51, 48, 55, 49};
 
 	while (*(s + count) != '\0')
 	{
