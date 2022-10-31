7345ea43825fc2f99fadc9701e525818e14e1ef9
Olajide Felix O <olajide.felix@yahoo.com>
2022-10-31 12:43:37 -0700

    Fixing error

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
  * _strcat- concatenates two strings
- * @dest: input string 1
- * @src: input string 2
+ * @dest: destination.
+ * @src: source
  * Return: dest
  */
 char *_strcat(char *dest, char *src)
@@ -16,9 +16,9 @@ char *_strcat(char *dest, char *src)
 	}
 	while (count2 >= 0)
 	{
-		*(dest + count) = *(src + count2)
-			if  (*(src + count2) == '\0')
-				break;
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
 		count++;
 		count2++;
 	}
