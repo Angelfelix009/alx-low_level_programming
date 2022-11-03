64ed59fc8dd2a7da7811b087de2f22cfa23a2e38
Olajide Felix O <olajide.felix@yahoo.com>
2022-11-03 13:35:44 -0700

    Recursion prints

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion- printsa a string
+ * @s: string
+ * Return: no return
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
