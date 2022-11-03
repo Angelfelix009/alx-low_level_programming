82de161c92dd4c4abc267b02c555b839100a4a01
Olajide Felix O <olajide.felix@yahoo.com>
2022-11-03 13:41:15 -0700

    error fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _print_rev_recursion - prints a string in reverse
+ * _print_rev_recursion - prints a string in reverse.
  * @s:string
  * Return: no return
  */
- void _print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
