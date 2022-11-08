8386c42aec216eea7670054555daeffcab139232
Olajide Felix O <olajide.felix@yahoo.com>
2022-11-08 01:06:06 -0800

    Adding file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- prints the multiplies of two numbers
+ * @argc: length of the arguments
+ * @argv: arguments passed during execution
+ * Return: success
+ */
+int main(int argc, char *argv)
+{
+	int i;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", i);
+	return (0);
+}
