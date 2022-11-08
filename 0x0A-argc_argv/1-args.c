af7d6aaf0a09090880ff00b5a35ef229311e33b4
Olajide Felix O <olajide.felix@yahoo.com>
2022-11-08 00:39:43 -0800

    adding file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- prints a number of arguments passed into it
+ * @argc: length of the argument passed
+ * @argv: arguments passed
+ * Return: success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
