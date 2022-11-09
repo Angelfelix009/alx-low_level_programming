75a02c8376a022ce0a90a13b3638f041bd1f2718
Olajide Felix O <olajide.felix@yahoo.com>
2022-11-09 04:57:04 -0800

    fixing error

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -r 0x09-static_libraries/0-isupper.c 0x09-static_libraries/0-strcat.c 0x09-static_libraries/1-isdigit.c 0x09-static_libraries/1-strncat.c 0x09-static_libraries/2-strlen.c 0x09-static_libraries/3-islower.c 0x09-static_libraries/3-strcmp.c 0x09-static_libraries/4-isalpha.c 0x09-static_libraries/5-strstr.c 0x09-static_libraries/9-strcpy.c 0x09-static_libraries/_putchar.c 0x09-static_libraries/0-memset.c 0x09-static_libraries/100-atoi.c 0x09-static_libraries/1-memcpy.c 0x09-static_libraries/2-strchr.c 0x09-static_libraries/2-strncpy.c 0x09-static_libraries/3-puts.c 0x09-static_libraries/3-strspn.c 0x09-static_libraries/4-strpbrk.c 0x09-static_libraries/6-abs.c
+cp -r 0-isupper.c 0-strcat.c 1-isdigit.c 1-strncat.c 2-strlen.c 3-islower.c 3-strcmp.c 4-isalpha.c 5-strstr.c 9-strcpy.c _putchar.c 0-memset.c 100-atoi.c 1-memcpy.c 2-strchr.c 2-strncpy.c 3-puts.c 3-strspn.c 4-strpbrk.c 6-abs.c
 gcc -c *.c
 ar -rc liball *.o
 ranlib liball.a
