4761bb4efaac9ec6a680cfbcb07361cc711a2fba
Olajide Felix O <olajide.felix@yahoo.com>
2022-12-07 09:33:51 -0800

    Adding file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * read_textfile: function that reads a text file
+ * read_textfile- function that reads a text file
  * and prints it to the POSIX standard output.
  * @filename: filename.
  * @letters:numbers of letters printed
@@ -22,8 +22,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	nrd = read(fd, buf, letters);
 	nwr = write(STDOUT_FILENO, buf, nrd);
+
 	close(fd);
 	free(buf);
+
 	return (nwr);
 }
 
